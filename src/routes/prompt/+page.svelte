978030f52e19d0fc3d935dc6e1b9b622921e9b3a
mashehu <mashehu3@gmail.com>
2023-10-26 09:25:33 +0200

    add countdown timer

--- src/routes/prompt/+page.svelte
+++ src/routes/prompt/+page.svelte
@@ -3,12 +3,14 @@
 	import { slide, fly } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
 
-	import { user, colorizedBackground } from '$lib/store';
+	import { user, colorizedBackground, theme } from '$lib/store';
 
 	import ActionBar from '$lib/layout/ActionBar.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import Loading from '$lib/components/Loading.svelte';
 	import Result from '$lib/components/Result.svelte';
+
+	import { onMount } from 'svelte';
 
 	$colorizedBackground = true;
 
@@ -36,6 +38,29 @@
 			}
 		};
 	};
+
+	let duration = 30; // in seconds
+	const circumference = 2 * Math.PI * 30; // 30 is the radius of the circle
+	let elapsed = 0;
+	let timeLeft = duration;
+	let circleColor: string;
+
+	onMount(() => {
+		circleColor = getComputedStyle(document.querySelector('main')).backgroundColor;
+		const interval = setInterval(() => {
+			if (elapsed < duration) {
+				elapsed += 1;
+				timeLeft = duration - elapsed;
+			} else {
+				// submit
+				document.getElementById('promptForm').requestSubmit();
+				clearInterval(interval);
+			}
+		}, 1000);
+		return () => clearInterval(interval); // Cleanup
+	});
+
+	$: offset = circumference * (1 - elapsed / duration);
 </script>
 
 <div class="flex min-h-screen flex-col">
@@ -49,7 +74,12 @@
 				class="h-full w-full px-6 pb-6 pt-14"
 			>
 				<label for="prompt">
-					<textarea name="prompt" class="prompt-input" bind:value={prompt} placeholder="Enter prompt" />
+					<textarea
+						name="prompt"
+						class="prompt-input"
+						bind:value={prompt}
+						placeholder="Enter prompt"
+					/>
 				</label>
 			</form>
 		{/if}
@@ -92,6 +122,45 @@
 				<div class="my-4 ml-4">
 					<p class="text-lg text-slate-900">{$user}</p>
 				</div>
+				<svg
+					width="70"
+					height="70"
+					viewbox="0 0 70 70"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<!-- Background circle -->
+					<circle
+						cx="35"
+						cy="35"
+						r="30"
+						fill="none"
+						stroke="lightgrey"
+						stroke-width="2"
+					/>
+
+					<!-- Countdown circle -->
+					<circle
+						cx="35"
+						cy="35"
+						r="30"
+						fill="none"
+						stroke={circleColor}
+						stroke-width="2"
+						stroke-dasharray={circumference}
+						stroke-dashoffset={offset}
+						transform="rotate(-90 35 35)"
+					/>
+
+					<!-- Time text in the center -->
+					<text
+						x="35"
+						y="45"
+						font-family="Arial"
+						font-size="30"
+						fill="black"
+						text-anchor="middle">{timeLeft}s</text
+					>
+				</svg>
 				<div class="ml-auto">
 					<Button label="Generate the image" type="submit" form="promptForm" />
 				</div>
